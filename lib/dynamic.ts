1df715df41367f5f344e109b445ca56671d0ddf2
Charles Lowell <cowboyd@frontside.com>
2026-08-28 15:09:58 -0500

    🚧 checkpoint dynamic route typing

---
+++ lib/dynamic.ts
@@ -0,0 +1,9 @@
+import type { WithRequirement, AnyRoute } from "./types.ts";
+
+
+export function dynamic<Before extends AnyRoute, Requirement, After extends AnyRoute>(
+  extension: (requires: Requirement) => (input: Before) => After,
+): (input: Before) => WithRequirement<After, Requirement> {
+  //@ts-expect-error;
+  return (id) => id;
+}
